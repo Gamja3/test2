4d57e83d88413f62b2607ce5385dea5b7981ada5
gamja3 <44pico@naver.com>
2022-10-26 00:50:43 +0900

    `123

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+
+const AntTabs = styled(Tabs)({
+  borderBottom: "2px solid #e8e8e8",
+  "& .MuiTabs-indicator": {
+    backgroundColor: "#e8e8e8",
+  },
+});
+
+const StyledTabs = styled((props) => (
+  <Tabs
+    {...props}
+    TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+  />
+))({
+  // borderBottom: "2px solid #e8e8e8",
+  "& .MuiTabs-indicator": {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+  },
+  "& .MuiTabs-indicatorSpan": {
+    maxWidth: 40,
+    width: "100%",
+    backgroundColor: "#fff",
+  },
+});
+
+const StyledTab = styled((props) => <Tab disableRipple {...props} />)(
+  ({ theme }) => ({
+    textTransform: "none",
+    fontSize: theme.typography.pxToRem(30),
+    marginRight: "100px",
+    color: "rgba(255, 255, 255, 0.7)",
+    "&.Mui-selected": {
+      color: "#fff",
+    },
+    "&.Mui-focusVisible": {
+      backgroundColor: "green",
+    },
+  })
+);
+
+function CustomizedTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ bgcolor: "#2e1534" }}>
+        <StyledTabs
+          value={value}
+          onChange={handleChange}
+          aria-label="styled tabs example"
+        >
+          <StyledTab label="Workflows1" />
+          <StyledTab label="Datasets" />
+          <StyledTab label="Connections" />
+        </StyledTabs>
+        <Box sx={{ p: 3 }} />
+      </Box>
+    </Box>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomizedTabs />
     </div>
   );
 }
